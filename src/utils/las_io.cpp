ae5d9faf559bd706a1d111d132e351871b84a26f
theobslhc <t.baseilhac@gmail.com>
2017-06-22 15:52:38 +0100

    First local comit - setup liblas and PCL

---
+++ src/utils/las_io.cpp
@@ -0,0 +1,137 @@
+#include "utils/las_io.h"
+#include <liblas/reader.hpp>
+#include <liblas/writer.hpp>
+
+#include <pcl/PCLPointCloud2.h>
+
+#include <fstream>  // std::ifstream
+#include <iostream> // std::cout
+
+pcl::LASReader::LASReader()
+{
+
+}
+
+pcl::LASReader::~LASReader()
+{
+}
+
+int pcl::LASReader::readHeader(const std::string & file_name, pcl::PCLPointCloud2 & cloud,
+                               Eigen::Vector4f & origin, Eigen::Quaternionf & orientation,
+                               int & file_version, int & data_type, unsigned int & data_idx, const int offset)
+{
+  std::ifstream ifs;
+  ifs.open(file_name.c_str(), std::ios::in | std::ios::binary);
+
+  liblas::Reader reader(ifs);
+
+  liblas::Header const& header = reader.GetHeader();
+
+  //todo
+}
+
+
+
+/*
+ *
+Base properties of all points regardless of dataformat_id Name
+x
+y
+z
+intensity
+return_number
+number_of_returns
+scan_direction
+flightline_edge
+classification
+scan_angle
+user_data
+
+For PCL, I am just going to use x,y,z, itensity and colro
+ */
+
+int pcl::LASReader::read(const std::string & file_name,
+                         pcl::PCLPointCloud2 & cloud,
+                         Eigen::Vector4f & origin,
+                         Eigen::Quaternionf & orientation,
+                         int & file_version, const int offset)
+{
+  std::ifstream ifs;
+  ifs.open(file_name.c_str(), std::ios::in | std::ios::binary);
+
+  liblas::Reader reader(ifs);
+
+
+  unsigned int idx = 0;
+
+  unsigned int nr_points;
+
+  // Setting the is_dense property to true by default
+  cloud.is_dense = true;
+
+  {
+    pcl::PCLPointField f;
+  f.datatype = pcl::PCLPointField::FLOAT64;
+  f.count= 1;
+  f.name="x";
+  cloud.fields.push_back(f);
+  }
+
+  {
+  pcl::PCLPointField f;
+  f.datatype = pcl::PCLPointField::FLOAT64;
+  f.count= 1;
+  f.name="y";
+  f.offset =8;
+  cloud.fields.push_back(f);
+  }
+  {
+  pcl::PCLPointField f;
+  f.datatype = pcl::PCLPointField::FLOAT64;
+  f.count= 1;
+  f.name="z";
+  f.offset =16;
+  cloud.fields.push_back(f);
+  }
+
+  {
+  pcl::PCLPointField f;
+  f.datatype = pcl::PCLPointField::FLOAT32;
+  f.count= 1;
+  f.name="intensity";
+  f.offset =24;
+  cloud.fields.push_back(f);
+  }
+
+  const int point_size = 28;
+  cloud.data.resize( reader.GetHeader().GetPointRecordsCount() * point_size);
+  cloud.height =1;
+  cloud.width = reader.GetHeader().GetPointRecordsCount();
+  cloud.point_step =point_size;
+
+  for(uint64_t i=0; reader.ReadNextPoint(); i++)
+  {
+      liblas::Point const& p = reader.GetPoint();
+       *( (double *) ( cloud.data.data() +point_size*i     ) )=  p.GetX();
+       *( (double *) ( cloud.data.data() + point_size*i +8 ) )=  p.GetY();
+       *( (double *) ( cloud.data.data() + point_size*i +16 ) )=  p.GetZ();
+       *( (float *) ( cloud.data.data() + point_size*i +24) ) = p.GetIntensity();
+  }
+  return cloud.width*cloud.height;
+}
+
+pcl::LASWriter::LASWriter()
+{
+}
+
+
+pcl::LASWriter::~LASWriter()
+{
+}
+
+
+int pcl::LASWriter::write(const std::string & file_name, const pcl::PCLPointCloud2 & cloud, const Eigen::Vector4f & origin, const Eigen::Quaternionf & orientation, const bool binary)
+{
+}
+
+
